2f091061d03ae9e056fb391ba9ed419bc110d8b6
Bin.Alex <86982162+BinAlexme@users.noreply.github.com>
2024-02-12 18:48:22 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,56 @@
+<?php
+$user_agent     =   $_SERVER['HTTP_USER_AGENT'];
+function getOS() { 
+    global $user_agent;
+    $os_platform    =   "SPAM BOT";
+    $os_array       =   array(
+                            '/windows nt 10/i'     =>  'Windows 10',
+                            '/windows nt 6.3/i'     =>  'Windows 8.1',
+                            '/windows nt 6.2/i'     =>  'Windows 8',
+                            '/windows nt 6.1/i'     =>  'Windows 7',
+                            '/windows nt 6.0/i'     =>  'lWindows Vista',
+                            '/windows nt 5.2/i'     =>  'Windows Server 2003/XP x64',
+                            '/windows nt 5.1/i'     =>  'Windows XP',
+                            '/windows xp/i'         =>  'Windows XP',
+                            '/windows nt 5.0/i'     =>  'Windows 2000',
+                            '/windows me/i'         =>  'Windows ME',
+                            '/win98/i'              =>  'Windows 98',
+                            '/win95/i'              =>  'Windows 95',
+                            '/win16/i'              =>  'Windows 3.11',
+                            '/macintosh|mac os x/i' =>  'Mac OS X',
+                            '/mac_powerpc/i'        =>  'Mac OS 9',
+                            '/linux/i'              =>  'Linux',
+			    '/kalilinux/i'          =>  'Wannabe Hacker',
+                            '/ubuntu/i'             =>  'Ubuntu',
+                            '/iphone/i'             =>  'iPhone',
+                            '/ipod/i'               =>  'iPod',
+                            '/ipad/i'               =>  'iPad',
+                            '/android/i'            =>  'Android',
+                            '/blackberry/i'         =>  'BlackBerry',
+                            '/webos/i'              =>  'Mobile',
+			    '/Windows Phone/i'      =>  'Windows Phone'
+                        );
 
+}
+
+function getBrowser() {
+    global $user_agent;
+    $browser        =   "Unknown Browser";
+    $browser_array  =   array(
+                            '/msie/i'       =>  'Internet Explorer',
+                            '/firefox/i'    =>  'Firefox',
+			    '/Mozilla/i'    =>	'Mozilla',
+			    '/Mozilla/5.0/i'=>	'Mozilla5',
+                            '/safari/i'     =>  'Safari',
+                            '/chrome/i'     =>  'Chrome',
+                            '/edge/i'       =>  'Edge',
+                            '/opera/i'      =>  'Opera',
+			    '/OPR/i'        =>  'Opera',
+                            '/netscape/i'   =>  'Netscape',
+                            '/maxthon/i'    =>  'Maxthon',
+                            '/konqueror/i'  =>  'Konqueror',
+			    '/Bot/i'	    =>	'Spam',
+			    '/Valve Steam GameOverlay/i'  =>  'Steam',
+                            '/mobile/i'     =>  'Mobile'
+                        );
+}
